4d88964634042d036e30da67896e7937d76f5c21
KatzCharland <cha18055@byui.edu>
2020-06-03 23:17:57 -0400

    commit

--- lesson7/lazyload.js/lazy2load.js
+++ lesson7/lazyload.js/lazy2load.js
@@ -1,17 +1,18 @@
 const images = document.querySelectorAll("[data-src]");
 
 function preloadImage(img) {
-    const src = img.getAttribute ("data-src");
+    const src = img.getAttribute("data-src");
         if(!src){
             return;
         }
         img.src = src;
+        img.removeAttribute;
 
 }
 
 const imgOptions = {
     threshold: 0,
-    rootMargin: "0px 0xp 300px 0px"
+    rootMargin: "0px 0px 200px 0px"
 };
 
 const imgObserver = new IntersectionObserver((entries, imgObserver) => {
@@ -27,6 +28,6 @@ const imgObserver = new IntersectionObserver((entries, imgObserver) => {
 
 }, imgOptions);
 
-images forEach(image => {
+images.forEach(image => {
     imgObserver.observe(image);
 });

23ada87cd446d34b42a47cf00f43388eb4cc4343
KatzCharland <cha18055@byui.edu>
2020-06-17 21:21:50 -0400

    commit

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -10,23 +10,23 @@ fetch(requestURL)
 
         for (let i = 0; i < prophets.length; i++) {
             let card = document.createElement('section');
-            let h2 = document.createElement('h2');
-            let birthdate = document.createElement('h4');
-            let birthPlace = document.createElement('h4');
+            let title = document.createElement('h2');
+            let birthDate = document.createElement('p');
+            let birthPlace = document.createElement('p');
             let image = document.createElement('img');
 
 
 
-           /* h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;*/
-            birthday.textContent = "Date of Birth: " + prophets[i].birthdate;
-            birthstate.textContent = "Place of Birth: " + prophets[i].birthplace;
+            title.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+            birthDate.textContent = 'Date of Birth: ' + prophets[i].birthDate;
+            birthPlace.textContent = 'Place of Birth: ' + prophets[i].birthPlace;
             image.setAttribute('src', prophets[i].imageurl);
+            image.setAttribute('alt', prophets[i].name + prophets[i].lastname + " - " + prophets[i].order);
 
-            card.appendChild(h2);
+            card.appendChild(title);
             card.appendChild(birthdate);
             card.appendChild(birthPlace);
             card.appendChild(image);
-
             document.querySelector('div.cards').appendChild(card);
         }
     });

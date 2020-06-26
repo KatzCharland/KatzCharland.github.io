e5a34004413c8991f31d548578fa4c4e482bee1b
KatzCharland <cha18055@byui.edu>
2020-06-26 12:36:45 -0400

    commit

---
+++ lesson-10/js/windChill.js
@@ -0,0 +1,13 @@
+(function () {
+    const high = document.getElementById('high').innerHTML;
+    const windSpeed = document.getElementById('windSpeed').innerHTML;
+    let windChill = 35.74 + (0.6215 * high) - (35.75 * (windSpeed ** .16)) + (0.4275 * high * (windSpeed ** .16));
+
+
+    if (high <= 50 && windSpeed > 3) {
+        windChill = Math.round(windChill);
+    } else {
+        windChill = " NA";
+    }
+    document.getElementById('windChill').innerHTML = windChill;
+}());
--- lesson10/js/weatherapi.js
+++
@@ -1,2 +0,0 @@
-const 'https://http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=
-626e9cefb93369cce66d9f4b2afe71fc'

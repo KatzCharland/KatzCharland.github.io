03f08d44d060cb753c773dc40fb29f2d2e66b87e
KatzCharland <cha18055@byui.edu>
2020-06-27 11:50:40 -0400

    commit

--- lesson-10/js/weatherapi.js
+++ lesson-10/js/weatherapi.js
@@ -1,16 +1,32 @@
-const apiURL = "http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=626e9cefb93369cce66d9f4b2afe71fc";
+const apiURL = 'http://api.openweathermap.org/data/2.5/weather?id=5604473&APPID=626e9cefb93369cce66d9f4b2afe71fc&units=imperial';
 fetch(apiURL)
-    .then((response) => response.json())
-    .then((jsObject) => {
-        console.log(jsObject);
-
-
- var weekcon = new Array(5);
+  
+ /*var weekcon = new Array(5)*/
 
      .then((response) => response.json())
-     .then((forecast) => {
-             console.log(forecast);
-             document.getElementById('day1').textContent = Math.round(forecast.list[0].main.temp_max);
+     .then((jsObject) => {
+              console.log(jsObject)
+              const currrentTemp = document.querySelector('#cuurnt-temp')
+              const icon =document.querySelector('img');
+
+              currrentTemp.textContent =jsObject.main.temp
+              
+              const imagesrc =`http://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`
+              const desc = jsObject.weather[0].description;
+
+             icon.setAttribute('src', imagesrc);
+             icon.setAttribute('alt', desc); 
+              
+
+
+ /* const apiURL = 'http://api.openweathermap.org/data/2.5/forecast?id=5604473&APPID=626e9cefb93369cce66d9f4b2afe71fc&units=imperial';
+fetch(apiURL)
+
+
+
+
+
+           document.getElementById('day1').textContent = Math.round(forecast.list[0].main.temp_max);
              document.getElementById('day2').textContent = Math.round(forecast.list[14].main.temp_max);
              document.getElementById('day3').textContent = Math.round(forecast.list[22].main.temp_max);
              document.getElementById('day4').textContent = Math.round(forecast.list[30].main.temp_max);
@@ -19,9 +35,10 @@ fetch(apiURL)
              weekcon[1] = forecast.list[14].weather[0].id;
              weekcon[2] = forecast.list[22].weather[0].id;
              weekcon[3] = forecast.list[30].weather[0].id;
-             weekcon[4] = parseInt(forecast.list[32].weather[0].id);
+             weekcon[4] = parseInt(forecast.list[32].weather[0].id);*/
 
-
+        
+    
 
 
 
@@ -34,3 +51,4 @@ fetch(apiURL)
 
     });
 
+/* console.log(forest)*/

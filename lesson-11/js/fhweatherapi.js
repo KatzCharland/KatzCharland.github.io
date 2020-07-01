55b7feabf95347054c3bbe90c75a0429a8df0d29
KatzCharland <cha18055@byui.edu>
2020-07-01 09:53:50 -0400

    commit

--- lesson-11/js/fhweatherapi.js
+++ lesson-11/js/fhweatherapi.js
@@ -1,4 +1,4 @@
-const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5607916&APPID=626e9cefb93369cce66d9f4b2afe71fc&units=imperial';
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?zipcode=83287APPID=626e9cefb93369cce66d9f4b2afe71fc&units=imperial';
 fetch(apiURL)
     .then((response) => response.json())
     .then((jsObject) => {

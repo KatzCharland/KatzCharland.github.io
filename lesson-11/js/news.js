40e5e2de76226a96ca64d9f4123e2b3f1b516b48
KatzCharland <cha18055@byui.edu>
2020-07-04 23:18:46 -0400

    commit

--- lesson-11/js/news.js
+++ lesson-11/js/news.js
@@ -19,7 +19,7 @@ fetch(requestURL)
         }
                                                                
                    
-        document.querySelector('.towns').appendChild(event_list);
+        document.getElementById("pEvents").innerHTML=events_list;
 
                     
     })

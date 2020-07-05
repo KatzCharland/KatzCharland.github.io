2d81cb1e3e96ff006716019957ddff7ca6540c6c
KatzCharland <cha18055@byui.edu>
2020-07-05 08:37:15 -0400

    commit

--- lesson5/js/friday.js
+++ lesson5/js/friday.js
@@ -3,5 +3,5 @@
      document.getElementById('pancakes').style.display='block';
  }
  else (
-     document.getElementById('pancakes').style.display = 'block';
- ) 
+     document.getElementById('pancakes').style.display = 'block');
+ 

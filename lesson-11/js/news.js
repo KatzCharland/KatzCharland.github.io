70b1f784880ec72c0a948caa3c0bb895d537dac2
KatzCharland <cha18055@byui.edu>
2020-07-04 23:01:51 -0400

    commit

---
+++ lesson-11/js/news.js
@@ -0,0 +1,25 @@
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        console.table(jsonObject); // temporary checking for valid response and data parsing
+        const towns = jsonObject['towns'];
+        //Creating elements and content
+        for (let i = 0; i < towns.length; i++) {
+            if (towns[i].name == "Preston") {
+                var event_list= "<ul>";
+                for (let t = 0; t < towns[i].events.length; t++) {
+                    event_list +="<li>"+ towns[i].events[t] + "</li>";  
+                }
+                 event_list += "</ul>"; 
+            }
+        }
+                                                               
+                   
+        document.querySelector('.towns').appendChild(card);
+
+                    
+    })

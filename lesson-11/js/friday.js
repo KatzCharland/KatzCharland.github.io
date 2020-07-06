3adbd2b5d598623cb406aa820e42ab83d17659da
KatzCharland <cha18055@byui.edu>
2020-07-05 22:36:48 -0400

    commit

--- lesson-11/js/friday.js
+++ lesson-11/js/friday.js
@@ -1,9 +1,9 @@
  let today = new Date();
  if (today.getDate() == 5){
-     document.getElementById('pancakes').style.display='show';
+     document.getElementById('pancakes').style.display='block';
  }
  else (
-     document.getElementById('pancakes').style.display = 'block');
+     document.getElementById('pancakes').style.display = 'show');
 
  
  

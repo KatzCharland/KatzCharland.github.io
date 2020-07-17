47b81f8683b0b52c76bf729190ed0176855b2572
KatzCharland <cha18055@byui.edu>
2020-07-17 00:22:03 -0400

    commit

--- Scoots4You/js/scoots.js
+++ Scoots4You/js/scoots.js
@@ -3,7 +3,7 @@ function circularText(txt, radius, classIndex) {
         classIndex = document.getElementsByClassName("circTxt")[classIndex];
 
     var deg = 360 / txt.length,
-        origin = 11;
+        origin = 270;
 
     txt.forEach((ea) => {
         ea = `<p style='height:${radius}px;position:absolute;transform:rotate(${origin}deg);transform-origin:0 100%'>${ea}</p>`;
@@ -12,4 +12,4 @@ function circularText(txt, radius, classIndex) {
     });
 }
 
-circularText("4 You               Scoot's ", 100, 0);
+circularText("Scoot's 4 You ", 100, 0);
